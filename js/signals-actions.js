035912ca47607172677984d2451e4210679963dd
smartmoneyman <icq261706687@gmail.com>
2025-12-29 20:19:11 +0200

    Update signals-actions.js

--- js/signals-actions.js
+++ js/signals-actions.js
@@ -1,1 +1,15 @@
+// js/signals-actions.js
+import { addToWatchlist } from './watchlist-api.js';
 
+document.addEventListener('click', e => {
+  const watch = e.target.dataset.watch;
+  if (watch) {
+    addToWatchlist(watch);
+    alert(`${watch} добавлен в Watchlist`);
+  }
+
+  const add = e.target.dataset.add;
+  if (add) {
+    window.location.href = `portfolio.html?symbol=${add}`;
+  }
+});
