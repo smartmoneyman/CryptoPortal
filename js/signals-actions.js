a924fcac2d21d008adcabf6b103fa8ca07cdaafa
smartmoneyman <icq261706687@gmail.com>
2025-12-29 20:28:44 +0200

    Update signals-actions.js

--- js/signals-actions.js
+++ js/signals-actions.js
@@ -1,15 +1,7 @@
-// js/signals-actions.js
-import { addToWatchlist } from './watchlist-api.js';
-
-document.addEventListener('click', e => {
-  const watch = e.target.dataset.watch;
-  if (watch) {
-    addToWatchlist(watch);
-    alert(`${watch} добавлен в Watchlist`);
-  }
-
-  const add = e.target.dataset.add;
-  if (add) {
-    window.location.href = `portfolio.html?symbol=${add}`;
+import {addToWatchlist} from './watchlist-api.js';
+document.addEventListener('click',e=>{
+  if(e.target.dataset.watch){
+    addToWatchlist(e.target.dataset.watch);
+    alert('Added to Watchlist');
   }
 });
