4c4ea3cf69a796c976b42f880a8d680184514e10
smartmoneyman <icq261706687@gmail.com>
2025-12-29 20:27:39 +0200

    Update watchlist-api.js

--- js/watchlist-api.js
+++ js/watchlist-api.js
@@ -1,1 +1,8 @@
+export function getWatchlist(){
+  return JSON.parse(localStorage.getItem('watchlist')||'[]');
+}
+export function addToWatchlist(s){
+  const w=getWatchlist();
+  if(!w.includes(s)){w.push(s);localStorage.setItem('watchlist',JSON.stringify(w))}
+}
 
