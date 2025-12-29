23ef665ce09a923ff7117a2952c1e205b1624e03
smartmoneyman <icq261706687@gmail.com>
2025-12-29 20:27:53 +0200

    Update watchlist.js

--- js/watchlist.js
+++ js/watchlist.js
@@ -1,1 +1,8 @@
+import {getWatchlist,addToWatchlist} from './watchlist-api.js';
+
+window.add=()=>{addToWatchlist(wl.value);render();}
+function render(){
+  watchlist.innerHTML=getWatchlist().map(s=>`<div>${s}</div>`).join('');
+}
+render();
 
